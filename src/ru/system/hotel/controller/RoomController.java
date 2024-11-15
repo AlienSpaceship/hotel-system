01e18dbf891ccf0a1acf43256b93fb64c578c709
AlienSpaceship <renatagverdievl3@gmail.com>
2024-11-15 23:27:14 +0300

    hotel-system

--- src/ru/system/hotel/controller/RoomController.java
+++ src/ru/system/hotel/controller/RoomController.java
@@ -1,5 +1,79 @@
 package ru.system.hotel.controller;
 
-public class RoomController {
+import ru.system.hotel.model.Type;
+import ru.system.hotel.service.RoomService;
 
+import java.util.Scanner;
+
+public class RoomController {
+    RoomService service = new RoomService();
+    Scanner scanner = new Scanner(System.in);
+
+    public void showMenu() {
+        while (true) {
+            System.out.println("\nМеню:");
+            System.out.println("1. Добавить комнату");
+            System.out.println("2. Удалить комнату");
+            System.out.println("3. Список комнат");
+            System.out.println("4. Выйти");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Очистка буфера
+
+            switch (choice) {
+                case 1:
+                    addRoom();
+                    break;
+                case 2:
+                    deleteRoom();
+                    break;
+                case 3:
+                    service.listRooms();
+                    break;
+                case 4:
+                    System.out.println("Выход из программы...");
+                    scanner.close();
+                    return; // Завершение метода, выход из цикла
+                default:
+                    System.out.println("Неверный выбор. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public void addRoom() {
+
+        String number;
+        Type roomType;
+        boolean isAvailable;
+
+        System.out.print("Введите номер комнаты: ");
+        number = scanner.nextLine();
+
+        while (true) {
+            try {
+                System.out.print("Введите тип комнаты (STANDARD, APARTMENT, PRESIDENTIAL): ");
+                roomType = Type.valueOf(scanner.nextLine().toUpperCase());
+                break; // Выход из цикла при успешном парсинге
+            } catch (IllegalArgumentException e) {
+                System.out.println("Некорректный тип комнаты. Пожалуйста, попробуйте снова.");
+            }
+        }
+
+        System.out.print("Доступна ли комната (true/false): ");
+        isAvailable = scanner.nextBoolean();
+        scanner.nextLine(); // Очистка буфера
+
+        service.addToDatabase(number, roomType, isAvailable);
+        System.out.println("Комната " + number +" успешно добавлена.");
+
+    }
+
+    public void deleteRoom(){
+        String number;
+        System.out.print("Введите номер комнаты который хотите удалить: ");
+        number = scanner.nextLine();
+
+        service.deleteFromDatabase(number);
+        System.out.println("Комната " + number +" успешно удалена.");
+    }
 }
--- src/ru/system/hotel/service/RoomService.java
+++ src/ru/system/hotel/service/RoomService.java
@@ -6,32 +6,30 @@ import static ru.system.hotel.database.Database.roomList;
 
 public class RoomService {
 
-    public boolean addToDatabase(String number, Type roomType, boolean isAvailable){
-        if(number == null || roomType == null){
-            System.out.println("Ошибка: Поле не заполнено");
-            return false;
-        }
-        try{
-            Room room = new Room(number, roomType, isAvailable);
-            roomList.add(room);
-            return false;
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
-            System.out.println("Ошибка при добавлении Room: " + ex.getMessage());
-            return false;
+    public void addToDatabase(String number, Type roomType, boolean isAvailable){
+        Room room = new Room(number, roomType, isAvailable);
+        roomList.add(room);
+        System.out.println("ADD ROOM...");
+    }
+
+    public void deleteFromDatabase(String number){
+        for(Room n : roomList){
+            if(n.getNumber().equals(number)){
+                roomList.remove(n);
+                System.out.println("DELETE ROOM...");
+                return;
+            }
         }
     }
 
-    public boolean deleteFromDatabase(String number){
-        for(Room room : roomList){
-        if(room.getNumber().equals(number)){
-            roomList.remove(room);
-            System.out.println("Удален: " + number);
-            return true;
+    public void listRooms() {
+        System.out.println("Список комнат:");
+        for (Room room : roomList) {
+            System.out.print("Номер: " + room.getNumber() + ", Тип: " + room.getRoomType() + ", Свободна: ");
+            if(room.isAvailable()){
+                System.out.println("Да");
+            }else System.out.println("Нет");
         }
-        }
-        System.out.println("Ошибка: комната " + number + " не найдена.");
-        return false; // Команата с таким номером не найдена
-    };
+    }
+
 }
